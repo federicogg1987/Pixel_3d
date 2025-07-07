5fdecbc4a5c1dc77678518907b9de7e71f2b6188
federico <federico.gg@hotmail.com>
2025-07-06 22:49:02 -0300

    Implementar imagen de fondo responsiva en Hero Section con CSS Media Queries

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const WHATSAPP_NUMBER = '5493512339940';
     // Tu número de WhatsApp con código de país, sin '+' ni espacios
     const WHATSAPP_NUMBER_CORDOBA = '5493512339940'; // ¡TU NÚMERO DE CÓRDOBA!
-    const WHATSAPP_NUMBER_LACALERA = '5493517654321'; // ¡NÚMERO DEL SOCIO DE LA CALERA!
+    const WHATSAPP_NUMBER_LACALERA = '5493543640927'; // ¡NÚMERO DE LA CALERA!
 
     // URL de Google Maps para incrustar (iframe)
     const MAP_URL_LACALERA = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3407.492072604228!2d-64.3423596253443!3d-31.34539773599267!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x942d63001bd98ca3%3A0xd06d02da0f0cceba!2sDelicioso%20paladar%20empanadas!5e0!3m2!1ses!2sar!4v1751852181161!5m2!1ses!2sar" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade'; // Reemplazar con tu URL real
